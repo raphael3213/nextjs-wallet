5d4b0ed745cee8acb0795fe4d2b5484fac4f5f3d
joel <raphael3213@gmail.com>
2023-11-20 06:38:30 +0530

    Remove console.log statements

--- hooks/useLocalStorageWallet.tsx
+++ hooks/useLocalStorageWallet.tsx
@@ -6,9 +6,7 @@ function useLocalStorageWallet() {
   const { data, isLoading, isError, isSuccess } = useQuery({
     queryKey: ["wallet"],
     queryFn: async () => {
-      console.log("IN wallet");
       if (localStorage.getItem("wallet")) {
-        console.log("got data", localStorage.getItem("wallet"));
         return localStorage.getItem("wallet")!;
       } else {
         return null;
